9cad4e5ea04a1ef039973aeea1ad6a4ce2ec2c22
ecrum19 <elicrum88@gmail.com>
2025-04-01 17:38:48 +0200

    updates to package.json, .gitignore, and config_permutations.py

--- experiments/experiment-automation.sh
+++ experiments/experiment-automation.sh
@@ -36,18 +36,17 @@ echo
 # -- With Service Clause workflow --
 
 echo "==> Starting With Service Clause workflow..."
-jbr init sparql-custom "default-service-${nextNumber}"
+npm run jbr init sparql-custom "default-service-${nextNumber}"
 mkdir "default-service-${nextNumber}/input/queries"
 python3 query_sort.py sib-swiss-federated-queries.json "default-service-${nextNumber}/" service
 
-cd "default-service${nextNumber}"
-jbr set-hook hookSparqlEndpoint sparql-endpoint-comunica
+cd "default-service-${nextNumber}"
+npm run jbr set-hook hookSparqlEndpoint sparql-endpoint-comunica
 cd ..
 
 python3 config_permutations.py "default-service-${nextNumber}/"
 
 cd "default-service-${nextNumber}"
-jbr generate-combinations
 
 npm run jbr -- prepare
 nohup npm run jbr -- run > "default-service-${nextNumber}.log" 2>&1 &
@@ -59,21 +58,21 @@ echo
 # -- No-Service Clause workflow --
 
 echo "==> Starting No-Service Clause experiments..."
-jbr init -c sparql-custom "no-service-${nextNumber}"
+npm run jbr init -c sparql-custom "no-service-${nextNumber}"
 mkdir "no-service-${nextNumber}/input/queries"
 python3 query_sort.py sib-swiss-federated-queries.json "no-service-${nextNumber}/" noservice
 
 cp -r ../config "no-service-${nextNumber}/input/config"
 
 cd "no-service-${nextNumber}"
-jbr set-hook hookSparqlEndpoint sparql-endpoint-comunica
+npm run jbr set-hook hookSparqlEndpoint sparql-endpoint-comunica
 cd ..
 
 mkdir "no-service-${nextNumber}/input/client-config"
 python3 config_permutations.py "no-service-${nextNumber}/input/config/"
 
 cd "no-service-${nextNumber}"
-jbr generate-combinations
+npm run jbr generate-combinations
 
 npm run jbr -- prepare
 nohup npm run jbr -- run > "no-service-${nextNumber}.log" 2>&1 &
