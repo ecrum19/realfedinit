e3611463b93a20eb97db7cf02ab516e4e4f64867
ecrum19 <elicrum88@gmail.com>
2025-04-08 12:41:50 +0200

    fixed configs and queries with uniprot source

--- experiments/cron_schedule.sh
+++ experiments/cron_schedule.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
 SCRIPT_PATH="~/realfedinit/experiments"
 SCRIPT_NAME="./start_experiment.sh"
@@ -29,9 +29,9 @@ function date_to_cron_fields() {
   local date="$1"
   local hour="$2"
   local minute="$3"
-  local day=$(date -d "$date" '+%-d')
-  local month=$(date -d "$date" '+%-m')
-  local dow=$(date -d "$date" '+%u')  # 1=Mon ... 7=Sun
+  local day=$(gdate -d "$date" '+%-d')
+  local month=$(gdate -d "$date" '+%-m')
+  local dow=$(gdate -d "$date" '+%u')  # 1=Mon ... 7=Sun
   echo "$minute $hour $day $month *"
 }
 
@@ -42,7 +42,7 @@ for range in "${MONTHLY_HALVES[@]}"; do
   current_date="$START_DATE"
 
   while [[ "$current_date" < "$END_DATE" || "$current_date" == "$END_DATE" ]]; do
-    weekday=$(date -d "$current_date" +%a)
+    weekday=$(gdate -d "$current_date" +%a)
 
     for week_key in "${!WEEKLY_QUARTERS[@]}"; do
       if [[ "${WEEKLY_QUARTERS[$week_key]}" == *"$weekday"* ]]; then
@@ -55,14 +55,14 @@ for range in "${MONTHLY_HALVES[@]}"; do
           cron_command="$cron_time cd $SCRIPT_PATH && $SCRIPT_NAME >> ~/realfedinit/experiments/cron-runs.log 2>&1"
 
           # Append to crontab
-          (crontab -l; echo "$cron_command") | crontab -
+          # (crontab -l; echo "$cron_command") | crontab -
           echo "Scheduled: $cron_command"
         done
       fi
     done
 
     # Advance one day
-    current_date=$(date -I -d "$current_date + 1 day")
+    current_date=$(gdate -I -d "$current_date + 1 day")
   done
 done
 
