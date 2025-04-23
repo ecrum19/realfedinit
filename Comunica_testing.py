3a4a8a30932a7f6f72b19db7234f8dfe22c5d353
ecrum19 <elicrum88@gmail.com>
2025-04-23 11:46:51 +0200

    rhea13

--- Comunica_testing.py
+++ Comunica_testing.py
@@ -12,24 +12,24 @@ def execute_queries(directory_path):
     - cli_command_template: Command with '{}' as placeholder for the query.
     """
     for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
-        sources = getSources(open(file_path, 'r'))
-        
-        # Format the CLI command
-        base_command = f"node ../comunica/engines/query-sparql/bin/query.js "
-        for source in sources:
-            if source != "":
-                fixed_source = source.replace('\n', '')
-                base_command += f"{fixed_source} "
-        print(base_command.split(" "))
-        
-        base_command += f"-f {file_path} -t stats"
-        try:
-            print(f"Executing: {base_command}")
-            result = subprocess.run(base_command, shell=True, check=True, text=True, capture_output=True)
-            print("Output:\n", result.stdout)
-        except subprocess.CalledProcessError as e:
-            print(f"Error executing command for {filename}: {e.stderr}")
+        if filename == "13_ns.rq":
+            file_path = os.path.join(directory_path, filename)
+            sources = getSources(open(file_path, 'r'))
+            
+            # Format the CLI command
+            base_command = f"node ../comunica/engines/query-sparql/bin/query.js "
+            for source in sources:
+                if source != "":
+                    fixed_source = source.replace('\n', '')
+                    base_command += f"{fixed_source} "
+            
+            base_command += f"-f {file_path} -t stats"
+            try:
+                print(f"Executing: {base_command}")
+                result = subprocess.run(base_command, shell=True, check=True, text=True, capture_output=True)
+                print("Output:\n", result.stdout)
+            except subprocess.CalledProcessError as e:
+                print(f"Error executing command for {filename}: {e.stderr}")
 
 def getSources(query_file):
     """
